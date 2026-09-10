57080ec8c017af70ed9d95c2e3e467be7d14fedb
Techforge25 <github@techforgeinnovations.com>
2026-09-10 11:33:23 +0600

    Initialize server

---
+++ src/server.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const connectDB = require("./database/connection");
+const startApp = require("./app");
+require("./workers/emailWorker");
+require("./workers/adminEmailWorker");
+
+(async () => {
+    try
+    {
+        // Connect db
+        await connectDB();
+
+        // Start app
+        await startApp();
+    }
+    catch(error)
+    {
+        console.log(error);
+    }
+})();
