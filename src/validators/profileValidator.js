f4ac54177e3ea387819d286efd8f7beceb0ebfef
Techforge25 <github@techforgeinnovations.com>
2026-09-14 18:41:41 +0600

    Create validator for updating a password

--- src/validators/profileValidator.js
+++ src/validators/profileValidator.js
@@ -9,4 +9,14 @@ const updateInfoValidator = joi.object({
     fullName: joi.string().trim().min(2).max(60).pattern(fullNamePattern).required().label("Full name"),
 });
 
-module.exports = { updateInfoValidator };
+// Update password validator
+const updatePasswordValidator = joi.object({
+    currentPassword: joi.string().required().label("Current password"),
+    newPassword: joi.string().min(8).max(128).pattern(new RegExp(passowrdPattern)).required().messages({
+        "string.pattern.base": "Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character.",
+        "string.min": "Password must be at least 8 characters long."
+    }).label("New password"),
+    confirmPassword: joi.string().valid(joi.ref("newPassword")).required().label("Confirm Password")    
+});
+
+module.exports = { updateInfoValidator, updatePasswordValidator };
